49f2af16c9863c9ee490385a2dca5d04b259cc83
Dasharath Sindhav <dasharathsindhav@Dasharaths-MacBook-Air.local>
2026-05-21 22:11:40 +0530

    data types

--- basics/data_type.py
+++ basics/data_type.py
@@ -1,3 +1,26 @@
 x = "Hello World"
+print(type(x))
+
+a = 20
+
+print(type(a))
+
+x = 20.5
+
+print(type(x))
+
+x = 1j
+
+print(type(x))
+
+x = ['apple', "banana", 1]
+
+print(type(x))
+
+x = ("apple", "banana", "mango")
+
+print(type(x))
+
+x = {"apple", "banana", "cherry"}
 
 print(type(x))
